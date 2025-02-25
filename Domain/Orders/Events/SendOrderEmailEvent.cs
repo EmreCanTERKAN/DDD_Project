b0e7d5692ae6cda5a6f33979fc35bcf417408e36
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-02-25 15:08:22 +0300

    Domain Events

---
+++ Domain/Orders/Events/SendOrderEmailEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Domain.Orders.Events;
+public sealed class SendOrderEmailEvent : INotificationHandler<OrderDomainEvent>
+{
+    public Task Handle(OrderDomainEvent notification, CancellationToken cancellationToken)
+    {
+        //mail sending logic
+        return Task.CompletedTask;
+    }
+}
